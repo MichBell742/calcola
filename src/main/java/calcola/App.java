8f12e585a03eacf3add2ca97af20a568a03d744f
Michele Bellucci1 <Michele Bellucci1>
2024-10-07 15:04:07 +0200

    second commit

--- src/main/java/calcola/App.java
+++ src/main/java/calcola/App.java
@@ -8,6 +8,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import org.apache.commons.jexl3.JexlBuilder;
+import org.apache.commons.jexl3.JexlContext;
+import org.apache.commons.jexl3.JexlEngine;
+import org.apache.commons.jexl3.JexlExpression;
+
 public class App extends Application{
 	TextField tOperazione=new TextField();
 	public void start(Stage finestra) {
@@ -20,12 +25,22 @@ public class App extends Application{
 		griglia.add(pulsante, 0, 1);
 		griglia.add(eRisultato, 0, 2);
 		
+		pulsante.setOnAction(e -> azione());
+		
 		Scene scena= new Scene(griglia);
 		
 		finestra.setTitle("opera");
 		finestra.setScene(scena);
 		finestra.show();
 	}
+	
+	public void azione(){
+		String operazione = tOperazione.getText();
+		 JexlEngine jexl = new JexlBuilder().create();
+		 JexlExpression e = jexl.createExpression( operazione );
+		 System.out.println(e.evaluate(null));
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
